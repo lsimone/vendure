4dd994cdc6c0352b4f17976ca7cc701ef566f548
Lorenzo Simone <lorenzo.simone@musement.com>
2019-07-20 14:24:21 +0200

    modular endpoints

---
+++ packages/dev-server/restPlugin/resolvers/auth.ts
@@ -0,0 +1,8 @@
+export const auth = () : object => {
+  console.warn('auth required')
+  // TODO: implement authentication
+  return {
+    "code": 200,
+    "result": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjcifQ.AsjbrBmU7phdYo6aXtSx2QI4bmY5ugz4zTUBmiXTsbs"
+  }
+}
--- packages/dev-server/restPlugin/rest.module.ts
+++ packages/dev-server/restPlugin/rest.module.ts
@@ -2,6 +2,7 @@ import { Module, Injectable, Controller, Get, Post } from '@nestjs/common';
 import { TaxRateService } from '@vendure/core';
 import { ServiceModule } from '@vendure/core/dist/service/service.module'
 import { EventBusModule } from '@vendure/core/dist/event-bus/event-bus.module'
+import { auth } from './resolvers/auth'
 // import { ServiceModule } from '../service/service.module';
 
 @Injectable()
@@ -25,12 +26,7 @@ export class AppController {
 
   @Post('auth/admin')
   auth(): object {
-    console.warn('auth required')
-    // TODO: implement authentication
-    return {
-      "code": 200,
-      "result": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjcifQ.AsjbrBmU7phdYo6aXtSx2QI4bmY5ugz4zTUBmiXTsbs"
-    }
+    return auth()
   }
 
 
