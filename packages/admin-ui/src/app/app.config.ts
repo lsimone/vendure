388f59b7c5596080289a22cc6ccc6c30d6c977e6
Lorenzo Simone <lorenzo.simone@musement.com>
2019-09-07 13:00:12 +0200

    feat(dev): Dev config for APIs

--- packages/admin-ui/src/app/app.config.ts
+++ packages/admin-ui/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { AdminUiConfig } from 'shared/shared-types';
 
+import { environment } from '../environments/environment';
+
 import { LanguageCode } from './common/generated-types';
 
 export const DEFAULT_LANGUAGE: LanguageCode = LanguageCode.en;
@@ -8,7 +10,10 @@ export const DEFAULT_CURRENCY = '£';
 let vendureUiConfig: AdminUiConfig | undefined;
 
 export function loadAppConfig(): Promise<void> {
-    return fetch('./vendure-ui-config.json')
+    return (environment.production
+        ? fetch('./vendure-ui-config.json')
+        : fetch('./vendure-ui-config.dev.json')
+    )
         .then(res => res.json())
         .then(config => {
             vendureUiConfig = config;
